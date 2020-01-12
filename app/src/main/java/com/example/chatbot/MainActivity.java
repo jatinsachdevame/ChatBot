7d1b970f7b51abfd0af377e0dfe56f6ab11a2fd8
Jatin <jatinsachdeva931@gmail.com>
2020-01-12 22:32:44 +0530

    Login and verification UI design

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.chatbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+        getActionBar().setTitle("ChatBot");
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.inflateMenu(R.menu.menu);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.home :
+                        Toast.makeText(MainActivity.this, "Well done", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
     }
+
+    public void test(View view) {
+        Toast.makeText(this, "Sent", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
 }
